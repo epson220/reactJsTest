81f2769cb488d7b97bb6bd3cde9ddf51f6f3ff8b
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-05 15:41:34 +0900

    LifeCycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -40,16 +40,46 @@ import ValidationSample from './ValidationSample';
 //   }
 // }
 
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
+//         <button onClick={() => this.scrollBox.scrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
       </div>
     );
   }
