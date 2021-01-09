b3bc81d31db3a49a27f597a9d984d9a6f27e9ed9
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-09 19:44:25 +0900

    Hooks end

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -49,24 +49,57 @@
 
 // export default Info;
 
-import React, { useReducer } from "react";
+// import React, { useReducer } from "react";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+// function reducer(state, action) {
+//   return {
+//     ...state,
+//     [action.name]: action.value,
+//   };
+// }
 
-const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+// const Info = () => {
+//   const [state, dispatch] = useReducer(reducer, {
+//     name: "",
+//     nickname: "",
+//   });
+//   const { name, nickname } = state;
+//   const onChange = (e) => {
+//     dispatch(e.target);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input name="name" value={name} onChange={onChange}></input>
+//         <input name="nickname" value={nickname} onChange={onChange}></input>
+//       </div>
+//       <div>
+//         <div>
+//           <b>이름:</b>
+//           {name}
+//         </div>
+//         <div>
+//           <b>닉네임:</b>
+//           {nickname}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Info;
+
+import React from "react";
+import useInputs from "./useInputs";
+
+const info = () => {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "",
   });
+
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
@@ -76,16 +109,13 @@ const Info = () => {
       </div>
       <div>
         <div>
-          <b>이름:</b>
-          {name}
+          <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b>
-          {nickname}
+          <b>닉네임:</b> {nickname}
         </div>
       </div>
     </div>
   );
 };
-
-export default Info;
+export default info;
