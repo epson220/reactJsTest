561b866a54e1f0e40118cf4f0b7b376ba15553ff
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-09 17:02:26 +0900

    useMemo

---
+++ hooks-tutorial/src/Counter.js
@@ -0,0 +1,28 @@
+import React, { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.vale - 1 };
+    default:
+      return state;
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{state.value}</b>
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
