a331b807d1da53cded257da1cf5cd33c6d5c82b7
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-15 23:11:07 +0900

    styled component

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,41 @@
+// import React, { Component } from "react";
+// import SassComponent from "./SassComponent";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <SassComponent></SassComponent>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// import React, { Component } from "react";
+// import CSSModule from "./CSSModule";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <CSSModule></CSSModule>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from "react";
-import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent></SassComponent>
+        <StyledComponent></StyledComponent>
       </div>
     );
   }
