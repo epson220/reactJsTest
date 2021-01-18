1fcfe509e50376e65b8832aea3e01c7576e1673c
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-19 00:33:54 +0900

    함수형 업데이트

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,19 +8,19 @@ import {
   useRef,
 } from '../node_modules/react/cjs/react.development';
 
-const App = () => {
-  function createBulkTodos() {
-    const array = [];
-    for (let i = 1; i <= 2500; i++) {
-      array.push({
-        id: i,
-        text: `할 일 ${i}`,
-        checked: false,
-      });
-    }
-    return array;
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
   }
+  return array;
+}
 
+const App = () => {
   // const [todos, setTodos] = useState([
   //   {
   //     id: 1,
@@ -32,38 +32,30 @@ const App = () => {
   // ]);
   const [todos, setTodos] = useState(createBulkTodos);
 
-  const nextId = useRef(2501);
+  const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    //setTodos(todos.concat(todo));
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
