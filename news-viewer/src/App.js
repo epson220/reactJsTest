95fac768d687b67b13dcf8ef4dcdab1756a21d3e
epson220 <62466439+epson220@users.noreply.github.com>
2021-01-28 00:01:23 +0900

    category

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -68,11 +68,19 @@
 
 // export default App;
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Categories from './components/Categories';
 import NewsList from './components/NewsList';
 
 const App = () => {
-  return <NewsList></NewsList>;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category}></NewsList>
+    </>
+  );
 };
 
 export default App;
