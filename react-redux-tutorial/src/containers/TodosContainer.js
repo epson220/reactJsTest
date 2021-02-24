3d3a5ccaf5f3f4883a676dea8910b055e653a762
epson220 <62466439+epson220@users.noreply.github.com>
2021-02-24 16:20:57 +0900

    웹요청 비동기 작업 처리

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 import { useSelector, useDispatch } from 'react-redux';
+import useActions from '../lib/useActions';
 
 // const TodosContainer = ({
 //   input,
@@ -23,18 +24,40 @@ import { useSelector, useDispatch } from 'react-redux';
 //     ></Todos>
 //   );
 // };
+// const TodosContainer = () => {
+//   const { input, todos } = useSelector(({ todos }) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }));
+//   const dispatch = useDispatch();
+//   const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
+//     dispatch,
+//   ]);
+//   const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+//   const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+//   const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={onChangeInput}
+//       onInsert={onInsert}
+//       onToggle={onToggle}
+//       onRemove={onRemove}
+//     ></Todos>
+//   );
+// };
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [
-    dispatch,
-  ]);
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
+  );
 
   return (
     <Todos
